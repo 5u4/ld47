d4327b67858a2bec2888acd217053040a7c2f94b
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-03 20:52:10 +0800

    Add camera

--- src/Scenes/World.cs
+++ src/Scenes/World.cs
@@ -9,12 +9,14 @@ namespace ld47.Scenes
     {
         public Position2D Spawn;
         public Node2D Corpses;
+        public Camera2D Camera;
 
         public override void _Ready()
         {
             base._Ready();
             Spawn = GetNode<Position2D>("Spawn");
             Corpses = GetNode<Node2D>("Corpses");
+            Camera = GetNode<Camera2D>("Camera2D");
 
             Emitter.Instance.Connect(nameof(Emitter.NewPlayerSignal), this, nameof(OnNewPlayer));
 
@@ -26,6 +28,7 @@ namespace ld47.Scenes
             var player = CreatePlayer();
             AddChild(player);
             player.GlobalPosition = Spawn.GlobalPosition;
+            AssignCamera(player);
         }
         
         public Player CreatePlayer()
@@ -38,6 +41,13 @@ namespace ld47.Scenes
         private void OnNewPlayer()
         {
             InitializePlayer();
+        }
+
+        private void AssignCamera(Node2D target)
+        {
+            var parent = Camera.GetParent();
+            parent?.RemoveChild(Camera);
+            target.AddChild(Camera);
         }
     }
 }
