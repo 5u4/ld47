e82cfdc19fc255d50a69234f708c0547c979bf2d
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-03 13:58:28 +0800

    Add player movement

--- src/Instances/Player/Player.cs
+++ src/Instances/Player/Player.cs
@@ -7,12 +7,14 @@ namespace ld47.Instances.Player
         public Vector2 Velocity = Vector2.Zero;
         public Animation Animation;
         public Gravity Gravity;
+        public Run Run;
 
         public override void _Ready()
         {
             base._Ready();
             Animation = GetNode<Animation>("Controllers/Animation");
             Gravity = GetNode<Gravity>("Controllers/Gravity");
+            Run = GetNode<Run>("Controllers/Run");
         }
 
         public override void _PhysicsProcess(float delta)
---
+++ src/Instances/Player/Run.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+namespace ld47.Instances.Player
+{
+    public class Run : Node2D
+    {
+        public Player Player;
+        public const float Speed = 120;
+        public const float Acceleration = 0.5f;
+        public const float Friction = 0.2f;
+        
+        public override void _Ready()
+        {
+            base._Ready();
+            Player = GetNode<Player>("../..");
+        }
+        
+        public override void _PhysicsProcess(float delta)
+        {
+            var direction = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+            Player.Velocity.x = Mathf.Abs(direction) > 0
+                ? Mathf.Lerp(Player.Velocity.x, direction * Speed, Acceleration)
+                : Mathf.Lerp(Player.Velocity.x, 0, Friction);
+        }
+    }
+}
