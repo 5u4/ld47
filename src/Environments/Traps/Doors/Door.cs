802839ee4cbab7b4b3f68f0196ba126bb3f8c517
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-04 15:00:23 +0800

    Add connected doors

--- src/Environments/Traps/Doors/Door.cs
+++ src/Environments/Traps/Doors/Door.cs
@@ -4,26 +4,31 @@ namespace ld47.Environments.Traps.Doors
 {
     public class Door : StaticBody2D
     {
+        [Export] public NodePath ConnectedDoorPath;
         public AnimatedSprite AnimatedSprite;
         public CollisionShape2D CollisionShape2D;
+        public Door ConnectedDoor;
         
         public override void _Ready()
         {
             base._Ready();
             AnimatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
             CollisionShape2D = GetNode<CollisionShape2D>("CollisionShape2D");
+            if (ConnectedDoorPath != null) ConnectedDoor = GetNode<Door>(ConnectedDoorPath);
         }
 
         public void TurnOn()
         {
             AnimatedSprite.Play("TurnOn");
             CallDeferred(nameof(Turn), true);
+            ConnectedDoor?.TurnOn();
         }
 
         public void TurnOff()
         {
             AnimatedSprite.Play("TurnOff");
             CallDeferred(nameof(Turn), false);
+            ConnectedDoor?.TurnOff();
         }
 
         private void Turn(bool onoff)
