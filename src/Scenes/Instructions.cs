bb81b74a2b328a69ddc3bb56c9b0426b02e569c8
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-05 17:11:54 +0800

    Fix instruction fading

--- src/Scenes/Instructions.cs
+++ src/Scenes/Instructions.cs
@@ -20,14 +20,15 @@ namespace ld47.Scenes
             base._Process(delta);
             UpdateFade();
             CountDown = Fading ? Mathf.Max(CountDown - delta, -2) : Mathf.Min(CountDown + delta, 1);
+            if (CountDown < -1.5) Fading = false; 
             Color.a = Mathf.Clamp(CountDown, 0, 1);
             Modulate = Color;
         }
 
         public void UpdateFade()
         {
-            Fading = Input.IsActionPressed("ui_left") || Input.IsActionPressed("ui_right") ||
-                     Input.IsActionPressed("ui_jump") || Input.IsActionPressed("ui_suicide");
+            if (Input.IsActionPressed("ui_left") || Input.IsActionPressed("ui_right") ||
+                Input.IsActionPressed("ui_jump") || Input.IsActionPressed("ui_suicide")) Fading = true;
         }
     }
 }
