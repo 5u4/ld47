b6ec7e25c67578b1fd3c1ccdeb41ee006b5d01be
Senhung Wong <alexwongsenhung@gmail.com>
2020-10-06 03:17:35 +0800

    Add title screen

---
+++ src/Scenes/Title.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+namespace ld47.Scenes
+{
+    public class Title : Node2D
+    {
+        public override void _Ready()
+        {
+            base._Ready();
+            GetNode<AnimatedSprite>("AnimatedSprite").Play();
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            base._Input(@event);
+            if (!(@event is InputEventKey)) return;
+            GetTree().ChangeScene("res://Scenes/Game.tscn");
+        }
+    }
+}
