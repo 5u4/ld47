96f99203a4ed0a5d1739deb7143d4e7f07c1dee8
Senhung Wong <alexwongsenhung@gmail.com>
2021-01-03 21:40:29 +0800

    Add icon

--- src/Scenes/Title.cs
+++ src/Scenes/Title.cs
@@ -2,19 +2,19 @@ using Godot;
 
 namespace ld47.Scenes
 {
-    public class Title : Node2D
-    {
-        public override void _Ready()
-        {
-            base._Ready();
-            GetNode<AnimatedSprite>("AnimatedSprite").Play();
-        }
+	public class Title : Node2D
+	{
+		public override void _Ready()
+		{
+			base._Ready();
+			GetNode<AnimatedSprite>("AnimatedSprite").Play();
+		}
 
-        public override void _Input(InputEvent @event)
-        {
-            base._Input(@event);
-            if (!(@event is InputEventKey)) return;
-            GetTree().ChangeScene("res://Scenes/Game.tscn");
-        }
-    }
+		public override void _Input(InputEvent @event)
+		{
+			base._Input(@event);
+			if (!(@event is InputEventKey)) return;
+			GetTree().ChangeScene("res://Scenes/Game.tscn");
+		}
+	}
 }
